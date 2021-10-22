6c68c9ae1e9cd01603f3977f4342e7546baea5d0
T-pl <tiagofqu@gmail.com>
2021-10-22 20:51:16 -0300

    Aplicando funcionalidades com JS

---
+++ funcionalidades.js
@@ -0,0 +1,14 @@
+
+const socialMedias = {
+    github: "T-pl",
+    youtube: "Tiago4849",
+    facebook: "tiago.fqu",
+    instagram = "camus_rebelde",
+    twitter = "tiagofqu"
+}
+
+
+function showMeShomehing(texto) {
+}
+
+showMeShomehing()
