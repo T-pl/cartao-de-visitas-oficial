147470d380e58c5227c0bded74d80474f8484759
T-pl <tiagofqu@gmail.com>
2021-10-26 22:46:20 -0300

    master: adicionei mais algumas funcionalidades

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,10 +1,10 @@
 
 const linksSocialMedias = {
+    linkedin: "in/tiago-ferreira-dev-web",
     github: "T-pl",
     youtube: "tiago4849",
     facebook: "tiago.fqu",
-    instagram: "camus_rebelde",
-    linkedin: "in/tiago-ferreira-dev-web"
+    instagram: "camus_rebelde"
 }
 
 function changeLinksSocialMedias() {
@@ -18,3 +18,15 @@ function changeLinksSocialMedias() {
 }
 
 changeLinksSocialMedias()
+
+function getInfosOfProfile() {
+    const url = `https://api.github.com/users/${linksSocialMedias.github}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            // bioDescrition.textContent = data.bio
+    })
+}
+
+getInfosOfProfile()
