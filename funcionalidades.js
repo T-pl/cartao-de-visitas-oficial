eed482eeb821e2f873a4fff8f96c8ad6842931b9
T-pl <tiagofqu@gmail.com>
2021-11-09 21:21:50 -0300

    Consertando Link Quebrado

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,6 +1,6 @@
 
 const linksSocialMedias = {
-    linkedin: "tiago-ferreira-dev-web",
+    linkedin: "/in/tiago-ferreira-dev-web",
     github: "T-pl",
     youtube: "tiago4849",
     facebook: "tiago.fqu",
