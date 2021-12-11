efe6490ae2addc6f0c0134425fd418048ea391d4
T-pl <tiagofqu@gmail.com>
2021-12-11 08:08:18 -0300

    MASTER: consertando bug do linkedin

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,21 +1,22 @@
 
-// const linksSocialMedias = {
-//     linkedin: "/in/tiago-ferreira-dev-web",
-//     github: "T-pl",
-//     youtube: "tiago4849",
-//     facebook: "tiago.fqu",
-//     instagram: "camus_rebelde"
-// }
+const linksSocialMedias = {
+    linkedin: "in/tiago-ferreira-dev-web",
+    github: "T-pl",
+    youtube: "tiago4849",
+    facebook: "tiago.fqu",
+    instagram: "camus_rebelde"
+}
 
-// function changeLinksSocialMedias() {
-//     for (let li of socialLinks.children) {
-//         const social = li.getAttribute('class')
+function changeLinksSocialMedias() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
 
-//         li.children[0].href = `https://${social}.com/${linksSocialMedias[social]}`
+        li.children[0].href = `https://${social}.com/${linksSocialMedias[social]}`
+        // https://linkedin.com/in/tiago-ferreira-dev-we
 
-//     }
+    }
 
-// }
+}
 
 changeLinksSocialMedias()
 
